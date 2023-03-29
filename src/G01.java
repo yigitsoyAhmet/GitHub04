7e8afbca106a7ab87ed4ea4e57f5db609d3c852a
AhmetYigitsoy <ahmetyigitsoy92@gmail.com>
2023-03-29 20:24:59 +0300

    4. gun

---
+++ src/G01.java
@@ -0,0 +1,5 @@
+public class G01 {
+    public static void main(String[] args) {
+        System.out.println("git/gitHub tekrar ve branch kullanımı");
+    }
+}
---
+++ src/G02.java
@@ -0,0 +1,12 @@
+public class G02 {
+
+    public static void main(String[] args) {
+        // 1- Proje ya da dosyalarımızı git init ile Git ile ilişkilendiriyoruz
+        //2- Versiyon olusturuyoruz git add . ve git commit -m"versiyonadi"
+        //3-gitHuba yollama git remote add origin repoUrlAdresi
+        //-already origin
+        //  -git remote set-url repoUrlAdresi
+        //-git push
+    }
+
+}
